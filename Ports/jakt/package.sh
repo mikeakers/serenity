d64f0206d5be1a586c53a73095356d6d585392c3
Mike Akers <mike.akers@gmail.com>
2022-08-12 02:07:59 -0400

    foasidhos

--- Ports/jakt/package.sh
+++ Ports/jakt/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=jakt
-version=d1e188a98502d3510c27ce82a9128f045d10073a
-files="https://github.com/SerenityOS/jakt/archive/d1e188a98502d3510c27ce82a9128f045d10073a.tar.gz jakt.tar.gz"
+version=6a9f52afcca1642a17cdb0037a08c0f3985ff43c
+files="https://github.com/SerenityOS/jakt/archive/${version}.tar.gz jakt.tar.gz"
 depends=("llvm")
 #configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DCMAKE_USE_SYSTEM_LIBRARY_LIBUV=1" "-DCMAKE_USE_OPENSSL=ON" "-GNinja")
 
@@ -15,4 +15,6 @@ build() {
 
 install() {
     run cp build/stage0 "${SERENITY_INSTALL_ROOT}/usr/local/bin/jakt"
+    run mkdir "${SERENITY_INSTALL_ROOT}/usr/local/include/jakt"
+    run cp -r runtime "${SERENITY_INSTALL_ROOT}/usr/local/include/jakt"
 }
